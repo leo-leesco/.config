77413c0a397c0b9cfbb261576c0c73ccb5f1e2e2
Léo LEESCO <leo.leesco@gmail.com>
2024-12-24 17:57:51 +0100

    initial commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Import the wezterm module
+local wezterm = require("wezterm")
+-- Creates a config object which we will be adding our config to
+local config = wezterm.config_builder()
+
+-- Import our new module (put this near the top of your wezterm.lua)
+local appearance = require("appearance")
+
+-- Use it!
+if appearance.is_dark() then
+	config.color_scheme = "Dracula+"
+else
+	config.color_scheme = "Atelierdune (light) (terminal.sexy)"
+end
+
+config.font = wezterm.font_with_fallback({
+	{
+		family = "Monaspace Neon",
+		harfbuzz_features = { "calt", "liga", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss08" },
+	},
+	"JetBrains Mono",
+	"Iosevka Term Extended",
+})
+config.font_size = 14
+-- Returns our config to be evaluated. We must always do this at the bottom of this file
+return config
