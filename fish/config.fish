947807b383f8e808cef93225d7723bf047406664
Léo LEESCO <leo.leesco@gmail.com>
2025-01-03 14:59:47 +0100

    added starfish prompt
    prompt = think of p10k

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ set LESSPIPE (which src-hilite-lesspipe.sh)
 set -x LESSOPEN "| $LESSPIPE %s"
 set -x LESS ' -R '
 
+set -x CLICOLOR 1
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
@@ -12,3 +13,5 @@ set -x LESS ' -R '
 # This section can be safely removed at any time if needed.
 test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
+
+starship init fish | source
