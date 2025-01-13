f6310487fca87d5db26cb0f163388271b8fe0bdb
Léo LEESCO <leo.leesco@gmail.com>
2025-01-13 09:52:11 +0100

    correctly adding homebrew to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-zoxide init --cmd cd fish | source
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 set -x EDITOR 'nvim'
 set LESSPIPE (which src-hilite-lesspipe.sh)
@@ -6,6 +6,7 @@ set -x LESSOPEN "| $LESSPIPE %s"
 set -x LESS ' -R '
 
 set -x CLICOLOR 1
+
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
@@ -14,4 +15,8 @@ set -x CLICOLOR 1
 test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
 
-starship init fish | source
+if status is-interactive
+	zoxide init --cmd cd fish | source
+
+	starship init fish | source
+end
