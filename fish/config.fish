4c8975f4754366c9c23a878e9fe0a953b44ff509
Léo LEESCO <leo.leesco@gmail.com>
2025-01-18 15:39:37 +0100

    make brew shell config arm/intel agnostic

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
-zoxide init --cmd cd fish | source
+if test -d /opt/homebrew/bin
+    eval (/opt/homebrew/bin/brew shellenv)
+else if test -d /usr/local/bin
+    eval (/usr/local/bin/brew shellenv)
+end
 
 set -x EDITOR 'nvim'
 set LESSPIPE (which src-hilite-lesspipe.sh)
@@ -6,12 +10,18 @@ set -x LESSOPEN "| $LESSPIPE %s"
 set -x LESS ' -R '
 
 set -x CLICOLOR 1
+
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
 test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+eval (opam env)
 # END opam configuration
 
-starship init fish | source
+if status is-interactive
+	zoxide init --cmd cd fish | source
+
+	starship init fish | source
+end
