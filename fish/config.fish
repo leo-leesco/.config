f76191e122978829afe25add4dcb39dd93fac1eb
Léo LEESCO <leo.leesco@gmail.com>
2025-01-30 22:49:49 +0100

    format config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ else if test -d /usr/local/bin
     eval (/usr/local/bin/brew shellenv)
 end
 
-set -x EDITOR 'nvim'
+set -x EDITOR nvim
 set LESSPIPE (which src-hilite-lesspipe.sh)
 set -x LESSOPEN "| $LESSPIPE %s"
 set -x LESS ' -R '
@@ -16,14 +16,14 @@ set -x CLICOLOR 1
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 eval (opam env)
 # END opam configuration
 
 if status is-interactive
-	zoxide init --cmd cd fish | source
+    zoxide init --cmd cd fish | source
 
-	starship init fish | source
+    starship init fish | source
 end
 
 fish_add_path -ag ~/.bun/bin
