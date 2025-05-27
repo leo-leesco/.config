ea85d8ad5cb7a97e8387afedc07b372d788e8169
Léo LEESCO <leo.leesco@gmail.com>
2025-05-27 11:37:41 -0700

    change colorschemes to nightfox ones

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -1,19 +1,19 @@
--- We almost always start by importing the wezterm module
 local wezterm = require("wezterm")
 -- Define a lua table to hold _our_ module's functions
 local module = {}
 
--- Returns a bool based on whether the host operating system's
--- appearance is light or dark.
-function module.is_dark()
-	-- wezterm.gui is not always available, depending on what
-	-- environment wezterm is operating in. Just return true
-	-- if it's not defined.
+-- Returns a bool based on whether the host operating system's appearance is light or dark.
+function module.is_light()
+	-- wezterm.gui is not always available, depending on what environment wezterm is operating in. Just return true if it's not defined.
 	if wezterm.gui then
-		-- Some systems report appearance like "Dark High Contrast"
-		-- so let's just look for the string "Dark" and if we find
-		-- it assume appearance is dark.
-		return wezterm.gui.get_appearance():find("Dark")
+		local appearance = wezterm.gui.get_appearance() -- this can be Light or Dark
+		wezterm.log_info(appearance)
+		local f = io.open(os.getenv("HOME") .. "/.cache/wezterm_appearance", "w")
+		if f then
+			f:write(appearance)
+			f:close()
+		end
+		return appearance:find("Light")
 	end
 	return true
 end
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,18 @@
--- Import the wezterm module
 local wezterm = require("wezterm")
 -- Creates a config object which we will be adding our config to
 local config = wezterm.config_builder()
 
 -- Import our new module (put this near the top of your wezterm.lua)
-local appearance = require("appearance")
+local colorscheme = require("appearance")
 
--- Use it!
-if appearance.is_dark() then
-	config.color_scheme = "catppuccin-mocha"
+config.color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors" }
+wezterm.log_info(config.color_scheme_dirs)
+if colorscheme.is_light() then
+	config.color_scheme = "dawnfox"
 else
-	config.color_scheme = "catppuccin-latte"
+	config.color_scheme = "nightfox"
 end
+wezterm.log_info(config.color_scheme)
 
 config.font = wezterm.font_with_fallback({
 	{
@@ -23,5 +24,5 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 14
 config.use_dead_keys = false
--- Returns our config to be evaluated. We must always do this at the bottom of this file
+
 return config
